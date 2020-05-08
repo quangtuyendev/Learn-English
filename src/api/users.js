af44887da7c08008c3cec6964633b5de4d7bae48
Nguyễn Quang Tuyền <quangtuyennguyen09@gmail.com>
2020-05-08 08:13:08 +0700

    [master] update code

--- src/api/users.js
+++ src/api/users.js
@@ -3,4 +3,4 @@ import axios from '../utils/axiosService';
 const END_POINT = 'users';
 
 export const getUsers = () => axios.get(`/${END_POINT}`);
-export const postUser = userInfo => axios.post(`/${END_POINT}`, userInfo);
+export const postUser = (userInfo) => axios.post(`/${END_POINT}`, userInfo);
--- src/api/words.js
+++ src/api/words.js
@@ -6,10 +6,10 @@ const END_POINT = 'words';
 export const getItems = () => axios.get(`/${END_POINT}`);
 
 // Post a new item
-export const postItem = word => axios.post(`/${END_POINT}`, word);
+export const postItem = (word) => axios.post(`/${END_POINT}`, word);
 
 // Update item
 export const putItem = (id, word) => axios.put(`/${END_POINT}/${id}`, word);
 
 // Detete a item
-export const deleteItem = id => axios.delete(`/${END_POINT}/${id}`);
+export const deleteItem = (id) => axios.delete(`/${END_POINT}/${id}`);
